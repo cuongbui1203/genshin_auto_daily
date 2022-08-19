086e9ded001b0d2d41527189517d21e06633b67f
kuon bui <cuongbuimanh1203@gmail.com>
2022-08-20 03:02:12 +0700

    sua nhe

---
+++ test.py
@@ -0,0 +1,7 @@
+import fbchat
+from fbchat import Client
+from getpass import getpass
+username = "cuongbuimanh1203.face@gmail.com"
+client = fbchat.Client(email=username, password='sWgh72^hdvJs~QyW')
+# if (client):
+#     print("thanh cong")
